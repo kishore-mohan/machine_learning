80cd1156df3683bd7dfdf491130dca3d37d637e4
kishore-mohan <kishore.mohan@jifflenow.com>
2018-03-15 23:54:57 +0530

    Data processing template added.

---
+++  Data Preprocessing /data_preprocessing_template.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 23:47:16 2018
+
+@author: kishore.mohan
+"""
+
+import numpy as mp
+import matplotlib.pyplot as plt
+import pandas as pd
