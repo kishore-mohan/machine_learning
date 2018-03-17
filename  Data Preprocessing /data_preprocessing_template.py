c7a06ed4a3bb131dec8ceff228bd493c3bc89944
kishore-mohan <kishore.mohan@jifflenow.com>
2018-03-17 19:01:21 +0530

    Kishore: data correction in pyton and R

---  Data Preprocessing /data_preprocessing_template.py
+++  Data Preprocessing /data_preprocessing_template.py
@@ -9,3 +9,12 @@ Created on Thu Mar 15 23:47:16 2018
 import numpy as mp
 import matplotlib.pyplot as plt
 import pandas as pd
+
+dataset = pd.read_csv("Data.csv")
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis= 0)
+imputer = imputer.fit(X[:, 1:])
+X[:, 1:3] = imputer.transform(X[: 1:3])
