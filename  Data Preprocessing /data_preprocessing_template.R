7cbe489fa3acb4329f3adcca9a5a2a4a1fd60642
kishore-mohan <kishore.mohan@jifflenow.com>
2018-03-22 22:25:07 +0530

    R feature scaling

---  Data Preprocessing /data_preprocessing_template.R
+++  Data Preprocessing /data_preprocessing_template.R
@@ -16,5 +16,8 @@ dataset$Purchased = factor((dataset$Purchased),
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
-training_set = susbset(dataset, split ==  TRUE)
+training_set = subset(dataset, split ==  TRUE)
 test_set = subset(dataset, split == FALSE)
+
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
